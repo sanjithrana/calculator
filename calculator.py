c05668a22ed7d22b49e70350e3b96d2a99417cc5
sanjith chilupuri <141348079+sanjithrana@users.noreply.github.com>
2024-02-15 22:56:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -20,20 +20,24 @@ operations = {
   "*": mul,
   "/": div
 }
+def rec():
+  n1 = float(input("what's the first number?: "))
+  for symbals in operations:
+    print(symbals)
+    
+  continue_1 = True
+  while continue_1:
+    operator = input("Pick an operation: ")
+    n2 = float(input("what's the next number?: "))
+    calculation = operations[operator]
+    result_1 = calculation(n1,n2)
+    print(f"{n1} {operator} {n2} = {result_1}")
+    if input(f"Type 'y' to continue calculation with {result_1},or type 'n' to start new: ") == "y":
+      n1 = result_1
+    else:
+      continue_1 = False
+      rec()
 
-n1 = float(input("what's the first number?: "))
-for symbals in operations:
-  print(symbals)
-  
-continue_1 = True
-while continue_1:
-  operator = input("Pick an operation: ")
-  n2 = float(input("what's the next number?: "))
-  calculation = operations[operator]
-  result_1 = calculation(n1,n2)
-  print(f"{n1} {operator} {n2} = {result_1}")
-  if input(f"Type 'y' to continue calculation with {result_1},or type 'n' to exit: ") == "y":
-    n1 = result_1
-  else:
-    continue_1 = False
-  
+
+
+rec()
